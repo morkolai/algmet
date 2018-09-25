6965ead4e22b46b5d98d5aedb69800da856efc9c
Nikolai <nikolmo@stud.ntnu.no>
2018-09-25 10:09:39 +0200

    initial commit

---
+++ queen.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+
+int const N = 4;
+int const LEN = N + 1;
+int pos[LEN];
+
+
+void dronning(int x);
+bool sjekkPoss(int x, int y);
+void display();
+
+int main() {
+
+	dronning(1);
+	display();
+
+	return 0;
+}
+
+bool sjekkPoss(int x, int y) {
+
+	for (int i = 1; i < LEN; i++) {					   //sjekker y-pos/kolonne
+		if (pos[i] = y) {
+			return false;
+		}
+	}
+
+	int yn = y, yo = y;						   //setter øvre og nedre diagonal
+
+	for (--x; x > 0; x--) {						  //går til venstre på brettet
+		
+		yn--;									//øvre og nedre diagonal sin y
+		yo++;
+
+		if (yn <= 0)				  //sikrer at det ikke går utenfor brettet
+			yn = 0;
+		if (yo <= 0)
+			yo = 0;
+
+		if (pos[x] == yn || pos[x] == yo)					//sjekker diagonal
+			return false;
+	}
+
+	return true;
+}
+
+void display() {
+	for (int i = 1; i < LEN; i++) {
+		std::cout << '(' << i << ', ' << pos[i] << ')' << std::endl;
+	}
+}
